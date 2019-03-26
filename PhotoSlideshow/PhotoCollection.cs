2a29571c88993a4887b9b2789a433f48c903d1ed
Sachin Jegaonkar <jegaonkarsachin@gmail.com>
2019-03-26 19:54:00 +0530

    Added a project
    Project along with the Problem statement for the Online Qualification Round of Google Hash Code 2019

---
+++ PhotoSlideshow/PhotoCollection.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoSlideshow
+{
+    public enum PhotoType
+    {
+        Horizontal,
+        Vertical
+    }
+
+    public class Photo
+    {
+        public PhotoType type;
+        public int tagCount;
+        public string[] tags;
+        public bool addedInSlideshow = false;
+    }
+
+    public class Slideshow
+    {
+        public long slideCount;
+        public List<long[]> slides;
+    }
+
+    public class PhotoCollection
+    {
+        long count;
+        List<Photo> Photos;
+
+        private PhotoCollection()
+        {
+        } // default constructor
+
+        public PhotoCollection(long photoCount, List<Photo> photos)
+        {
+            this.count = photoCount;
+            this.Photos = photos;
+        }
+
+        public Slideshow MakeSlideshow()
+        {
+            Slideshow slideshow = new Slideshow();
+
+            slideshow.slides = new List<long[]>();
+
+            for (int i = 0; i < Photos.Count; i++)
+            {
+                if (Photos[i].addedInSlideshow && Photos[i].type != PhotoType.Vertical) continue;
+
+                if (Photos[i].type == PhotoType.Horizontal ||
+                    (Photos[i].type == PhotoType.Vertical && !Photos[i].addedInSlideshow))
+                {
+                    Photos[i].addedInSlideshow = true;
+
+                    Photo neighbor;
+                    FindHorizontalNeighboringSlide(Photos[i], out neighbor);
+                    if (neighbor != null)
+                    {
+                        if (Photos[i].type == PhotoType.Vertical)
+                            slideshow.slides.Add(new long[] { i, Photos.IndexOf(neighbor) });
+                        else
+                            slideshow.slides.Add(new long[] { Photos.IndexOf(neighbor) });
+                        Photos[Photos.IndexOf(neighbor)].addedInSlideshow = true;
+                    }
+                    else
+                    {
+                        Photos[i].addedInSlideshow = false;
+                    }
+                }
+                else
+                {
+                    Photos[i].addedInSlideshow = true;
+
+                    Photo neighbor;
+                    FindVerticalNeighboringSlide(Photos[0], out neighbor);
+                    if (neighbor != null)
+                    {
+                        if (neighbor.type == PhotoType.Horizontal)
+                        slideshow.slides.Add(new long[] { Photos.IndexOf(neighbor) });
+                        else
+                            slideshow.slides.Add(new long[] { i, Photos.IndexOf(neighbor) });
+
+                        Photos[Photos.IndexOf(neighbor)].addedInSlideshow = true;
+                    }
+                    else
+                    {
+                        Photos[i].addedInSlideshow = false;
+                    }
+                }
+            }
+
+            slideshow.slideCount = slideshow.slides.Count;
+
+            return slideshow;
+        }
+
+        private void FindHorizontalNeighboringSlide(Photo slide, out Photo neighbor)
+        {
+            // Horizontal or vertical photo
+            neighbor = null;
+
+            /*
+             * the number of common tags between S i and S i+1
+             * the number of tags in S i but not in S i+1
+             * the number of tags in S i+1 but not in S i .
+             */
+
+            neighbor = Photos.Where(p =>
+            {
+                if (p.addedInSlideshow) return false;
+                int commonTags = slide.tags.Count(x => p.tags.Contains(x));
+                int tagsInSiButNotInSii = slide.tags.Count(x => !p.tags.Contains(x));
+                int tagsInSiiButNotInSi = p.tags.Count(pp => !slide.tags.Contains(pp));
+
+                return (commonTags > 1 && tagsInSiButNotInSii > 1 && tagsInSiiButNotInSi > 1);
+            }).FirstOrDefault();
+
+            // neighbor;
+        }
+
+
+        private void FindVerticalNeighboringSlide(Photo slide, out Photo neighbor)
+        {
+            neighbor = null;
+
+            /*
+             * the number of common tags between S i and S i+1
+             * the number of tags in S i but not in S i+1
+             * the number of tags in S i+1 but not in S i .
+             */
+
+            neighbor = Photos.Where(p =>
+            {
+                if (p.addedInSlideshow || p.type == PhotoType.Horizontal) return false;
+                int commonTags = slide.tags.Count(x => p.tags.Contains(x));
+                int tagsInSiButNotInSii = slide.tags.Count(x => !p.tags.Contains(x));
+                int tagsInSiiButNotInSi = p.tags.Count(pp => !slide.tags.Contains(pp));
+
+                return (commonTags > 1 && tagsInSiButNotInSii > 1 && tagsInSiiButNotInSi > 1);
+            }).FirstOrDefault();
+
+            // neighbor; anotherNeighbor;
+        }
+    }
+}
---
+++ PhotoSlideshow/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoSlideshow
+{
+    static class Program
+    {
+        public static PhotoCollection ReadFromInputFile(string inputFile)
+        {
+            long N;
+
+            List<Photo> photos = new List<Photo>();
+
+            if (!File.Exists(inputFile))
+            {
+                throw new Exception("Cannot find the input file.");
+            }
+
+            var fileStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read);
+            if (fileStream == null)
+            {
+                throw new Exception("Cannot open the input file.");
+            }
+
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                var parameters = streamReader.ReadLine().Split(' ');
+                long.TryParse(parameters[0], out N);
+
+                if (!(1 <= N && N <= Math.Pow(10, 5)))
+                {
+                    throw new System.Exception("The input parameters are not in a valid range.");
+                }
+
+                for (long i = 0; i < N; ++i)
+                {
+                    char type; int tagCount;
+                    Photo photo = new Photo();
+
+                    var photoDescription = streamReader.ReadLine().Split(' ');
+                    char.TryParse(photoDescription[0], out type);
+                    photo.type = (type == 'H') ? PhotoType.Horizontal : PhotoType.Vertical;
+                    int.TryParse(photoDescription[1], out tagCount);
+                    photo.tagCount = tagCount;
+                    if (!(1 <= tagCount && tagCount <= 100))
+                    {
+                        throw new System.Exception("The input parameters are not in a valid range.");
+                    }
+                    photo.tags = new string[tagCount];
+                    for (int t = 2, p = 0; t < photoDescription.Length; t++, p++)
+                        photo.tags[p] = photoDescription[t];
+
+                    photos.Add(photo);
+                }
+            }
+
+            if ((long)photos.Count != N)
+            {
+                throw new System.Exception("The pizza data is not in a valid range.");
+            }
+
+            return new PhotoCollection(N, photos);
+        }
+
+        public static void WriteToOutputFile(Slideshow slideShow, string outputFile)
+        {
+            if (slideShow.slides == null)
+                return;
+
+            var fileStream = new FileStream(outputFile, FileMode.CreateNew, FileAccess.Write);
+            if (fileStream == null)
+            {
+                throw new Exception("Cannot open the output file.");
+            }
+
+            using (var streamWriter = new StreamWriter(fileStream))
+            {
+                streamWriter.WriteLine(slideShow.slideCount);
+
+                foreach (var slide in slideShow.slides)
+                {
+                    streamWriter.WriteLine(String.Join(" ", slide));
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Write("Usage: PhotoSlideshow <input_file_path> <output_file_path>");
+                Console.Write("\n");
+                return;
+            }
+
+            try
+            {
+                PhotoCollection collection = ReadFromInputFile(args[0]);
+                var slideShow = collection.MakeSlideshow();
+                WriteToOutputFile(slideShow, args[1]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return;
+        }
+    }
+}
